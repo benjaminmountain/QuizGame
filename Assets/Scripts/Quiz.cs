7e92b2781f1d37aff96c1227f977ee0f696dfafa
Ben Mountain <ben.mountain@perficient.com>
2022-07-16 18:43:24 -0400

    First commit

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour {
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    private void Start() {
+
+    }
+}
